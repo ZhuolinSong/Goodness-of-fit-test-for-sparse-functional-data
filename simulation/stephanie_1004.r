8ffb298e3f23945c69d04e846af012322817db43
Charlie <zsong6@ncsu.edu>
2021-08-17 12:03:54 -0400

    bug fixed

--- simulation/stephanie_1004.r
+++ simulation/stephanie_1004.r
@@ -6,5 +6,5 @@ s_k <- 5000
 s_n <- 100
 s_m <- 4
 
-stephanie_5004 <- stephanie_type1(seed, s_k, s_n, s_m, L = 1000)
-save(stephanie_5004, file = "stephanie_5004.RData")
+stephanie_1004 <- stephanie_type1(seed, s_k, s_n, s_m, L = 1000)
+save(stephanie_1004, file = "stephanie_1004.RData")
--- simulation/stephanie_5004.r
+++ simulation/stephanie_5004.r
@@ -3,8 +3,8 @@ RNGkind("L'Ecuyer-CMRG")
 
 seed <- 999983
 s_k <- 5000
-s_n <- 100
+s_n <- 500
 s_m <- 4
 
-stephanie_1004 <- stephanie_type1(seed, s_k, s_n, s_m, L = 1000)
-save(stephanie_1004, file = "stephanie_1004.RData")
+stephanie_5004 <- stephanie_type1(seed, s_k, s_n, s_m, L = 1000)
+save(stephanie_5004, file = "stephanie_5004.RData")
