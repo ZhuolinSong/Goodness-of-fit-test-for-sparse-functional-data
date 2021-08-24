9b1eb3c1704157083680ba4a15283e56d2293378
Zhuolin Song <zsong7@ncsu.edu>
2021-08-24 14:11:13 -0400

    multi-thread 1

--- simulation/steface/steface_1004.r
+++ simulation/steface/steface_1004.r
@@ -1,10 +1,15 @@
+library(parallel)
 source("simulation.R")
 RNGkind("L'Ecuyer-CMRG")
 
-seed <- 999983
+s_seed <- 999983
 s_k <- 5000
 s_n <- 100
 s_m <- 4
+v_seed <- c(s_seed, s_seed + s_k)
 
-steface_1004 <- stephanie_type1(seed, s_k, s_n, s_m, L = 1000, i_face = T)
+steface_1004 <- mclapply(v_seed, seed_loop <- function(seed) {
+    stephanie_type1(seed, s_k / 2, s_n, s_m, L = 1000, i_face = T)
+}, mc.cores = 2)
+
 save(steface_1004, file = "steface_1004.RData")
